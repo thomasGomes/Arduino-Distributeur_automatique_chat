b20b9fb21617473e4e6e94bf182d256a3941455e
thomasGomes <thomas.henry.gomes@gmail.com>
2021-05-11 22:14:46 +0200

    Update Arduino-Distributeur_automatique_chat.ino

--- Arduino-Distributeur_automatique_chat.ino
+++ Arduino-Distributeur_automatique_chat.ino
@@ -5,23 +5,37 @@
 // to get the number of steps (e.g. 360 / 3.6 gives 100 steps).
 const int stepsPerRevolution = 2048; // 32*64 = 2048
 
+// Time
+unsigned long myTime;
+unsigned long stepTime;
+
+// Reset once
+bool resetNeeded;
+
 // Pin for capactitive touch sensor
-#define ctsPin 2 
+#define ctsPin 2
 
 // Initialize with pin sequence IN1-IN3-IN2-IN4 with 28BYJ-48 stepper motor
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
 
 void setup() {
   // The speed at which the motor should turn in rotations per minute - a positive number
-  myStepper.setSpeed(8);
+  myStepper.setSpeed(14);
+
+  // Avoid reset when no interaction
+  resetNeeded = false;
 }
 
 void loop() {
   int ctsValue = digitalRead(ctsPin);
-  
-  if (ctsValue == HIGH){
-    myStepper.step(-stepsPerRevolution*4);
+  myTime = millis();
 
+  if (ctsValue == HIGH) {
+    myStepper.step(-stepsPerRevolution / 8);
+    resetNeeded = true;
+    stepTime = millis();
+  }
+  else if (resetNeeded && (myTime > stepTime + 2000)) {
     reset();
   }
 }
@@ -29,4 +43,5 @@ void loop() {
 void reset() {
   // Pin 12 must be connected to Reset pin
   pinMode(12, OUTPUT);
+  resetNeeded = false;
 }
